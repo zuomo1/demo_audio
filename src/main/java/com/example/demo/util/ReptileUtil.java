b93d6d93197e7e4cbe7e29c45b256ebfefcf686e
taiwenqi <1057646680@qq.com>
2019-09-16 11:18:21 +0800

    update

--- src/main/java/com/example/demo/util/ReptileUtil.java
+++ src/main/java/com/example/demo/util/ReptileUtil.java
@@ -24,7 +24,7 @@ public class ReptileUtil {
         }
         Document doc;
         try {
-            doc = Jsoup.connect(url + setNumber).get();
+            doc = Jsoup.connect(url + setNumber).header("User-Agent", "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.4; en-US; rv:1.9.2.2) Gecko/20100316 Firefox/3.6.2").get();
             Element element = doc.getElementsByAttributeValue("class", "h-play").first();
             Elements h1s = element.getElementsByTag("h1");
             Element element1 = h1s.get(0);
